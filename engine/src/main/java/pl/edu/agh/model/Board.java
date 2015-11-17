c02eb54399e5a8197a87231e6b50f6e1c86b2384
Michal Kostrzewa <michal.kostrzewa@comarch.pl>
2015-11-17 18:33:43 +0100

    [MECHANIKA GRY] Nowy model planszy

---
+++ engine/src/main/java/pl/edu/agh/model/Board.java
@@ -0,0 +1,33 @@
+package pl.edu.agh.model;
+
+import java.security.Timestamp;
+
+/**
+ * Created by mkostrzewa on 2015-11-17.
+ */
+public class Board {
+    private Timestamp timeofGame;
+    private int height = 20;
+    private int width = 20;
+    private int[][] tabBoard = new int[height][width];
+
+    public void displayOrCleanTabBoard(boolean clean) {
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                if (clean) {
+                    tabBoard[i][j] = 0;
+                } else {
+                    System.out.print(tabBoard[j][i] + " ");
+                }
+            }
+            if (!clean) System.out.println("");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.displayOrCleanTabBoard(true);
+        board.displayOrCleanTabBoard(false);
+    }
+}
