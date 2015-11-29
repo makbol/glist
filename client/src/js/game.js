9fcc5e102147583621c9726394cacaee8b0dfb90
Piotr Skibiak <piotr.skibiak@gmail.com>
2015-11-29 15:42:20 +0100

    add page update using server location

--- client/src/js/game.js
+++ client/src/js/game.js
@@ -1,28 +1,31 @@
 (function() {
   'use strict';
+  
+  var playerToIdMap = {}
 
-
-  var players = [{
-    'x' : 1,
-    'y' : 1,
-    'v_x' : 1,
-    'v_y' : 1,
-    'color' : 'red'
-  }]
+  var getDistance = function(x1, y1, x2, y2) {
+    return Math.sqrt( Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
+  }
 
   function Game() {}
-
 
   Game.prototype = {
     create: function () {
       this.input.onDown.add(this.onInputDown, this);
-      this.game.add.tileSprite(0, 0, 1920, 1920, 'background');
+      this.game.add.tileSprite(0, 0, 1920, 1920, 'background'); 
       this.game.world.setBounds(0, 0, 1920, 1920);
       this.game.physics.startSystem(Phaser.Physics.P2JS);
       player = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
       this.game.physics.p2.enable(player);
       this.cursors = this.game.input.keyboard.createCursorKeys();
       this.game.camera.follow(player);
+
+      for (var tmpPlayer in playersList) {
+        var tmpPlayerObject = playersList[tmpPlayer];
+        playerToIdMap[tmpPlayerObject.id] = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'player');
+        this.game.physics.p2.enable(playerToIdMap[tmpPlayerObject.id]);
+      }
+
     },
 
     update: function () {
@@ -37,6 +40,26 @@
         player.body.velocity.x = -300;
       } else if (this.cursors.right.isDown) {
         player.body.moveRight(300);
+      }
+
+      if(ws.readyState != 0) {
+        ws.send( {'id' : playerId, 
+                   'x' : player.body.x, 
+                   'y' : player.body.y,  
+                   'v_x' : player.body.velocity.x, 
+                   'v_y' : player.body.velocity.y } );
+      }
+      for (var tmpPlayer in playersList) {
+        var tmpPlayerObject = playersList[tmpPlayer];
+
+        playerToIdMap[tmpPlayerObject.id].body.velocity.x = playersList[tmpPlayer].v_x;
+        playerToIdMap[tmpPlayerObject.id].body.velocity.y = playersList[tmpPlayer].v_y;
+        
+        if (getDistance(playerToIdMap[tmpPlayerObject.id].body.x, playerToIdMap[tmpPlayerObject.id].body.y,
+                        playersList[tmpPlayer].x, playersList[tmpPlayer].y) > 10) {
+          playerToIdMap[tmpPlayerObject.id].body.x = playersList[tmpPlayer].x;
+          playerToIdMap[tmpPlayerObject.id].body.y = playersList[tmpPlayer].y;
+        }
       }
     },
 
--- client/src/js/main.js
+++ client/src/js/main.js
@@ -2,7 +2,25 @@
 
 var player;
 
+var playerId; 
+
 var GAME_SERVER_ADDRESS = "ws://10.22.107.19:1666";
+
+var playersList =  [{
+    'x' : 700,
+    'y' : 700,
+    'v_x' : 5,
+    'v_y' : 5,
+    'color' : 'red',
+    'id' : 1000
+  }, {
+    'x' : 800,
+    'y' : 800,
+    'v_x' : 2,
+    'v_y' : 2,
+    'color' : 'red',
+    'id' : 1100
+  }]
 
 function makeid()
 {
@@ -15,27 +33,33 @@ function makeid()
     return text;
 }
 
+var ws;
+
 window.addEventListener('load', function () {
 
    var ns = window['tron'];
-   var ws;
 
    function initiateWebSocketConnection() {
       if ("WebSocket" in window) {
          ws = new WebSocket(GAME_SERVER_ADDRESS);
          
-         ws.onopen = function() {
-         	var id = makeid();
-            ws.send(id);
+         ws.onopen = function(event) {
+         	  var id = makeid();
+            ws.send("joinGame," + id);
          };
          
          ws.onmessage = function (evt) { 
             var received_msg = JSON.parse(evt.data);
-            switch (received_msg.commandType) {
-  			    case "START":
-  			        break;
+            switch (received_msg.commandName) {
+  			    case "joinGame":
+              playerId = received_msg.result;
+              break;
+            case "START":
+              game.state.start('game');
+  			      break;
   			    case "UPDATE":
-  			        break;
+              playersList = received_msg.data;
+  			      break;
   			    case "GAME_OVER":
   			    	break;
 			     }
