1962438d7d6689bb4ea543051e46ab5d2015d20e
Bartosz Sądel <bsadel69@gmail.com>
2015-12-01 19:52:29 +0100

    update command

--- engine/src/main/java/pl/edu/agh/commands/StartNewGameCommand.java
+++ engine/src/main/java/pl/edu/agh/commands/StartNewGameCommand.java
@@ -1,10 +1,5 @@
 package pl.edu.agh.commands;
 
-import com.google.gson.Gson;
-import java.util.Properties;
-import pl.edu.agh.core.BaseCommand;
-import static pl.edu.agh.core.BaseCommand.COMMAND_NAME_PARAM;
-import static pl.edu.agh.core.BaseCommand.RESULT_PARAM;
 import pl.edu.agh.core.BroadcastCommand;
 import pl.edu.agh.core.Room;
 import pl.edu.agh.model.Player;
@@ -16,8 +11,7 @@ public class StartNewGameCommand extends BroadcastCommand {
 
     /** Nazwa komendy */
     public static final String COMMAND_NAME = "startNewGame";
-    
-    private GetActiveUsersListCommand cmd;
+
     private Player executor;
 
     public StartNewGameCommand(String[] params) {
